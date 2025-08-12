d44c6060cab0bd7f96403fbb9f06ab59c1143ab9
Amanda <2952582445@qq.com>
2025-08-12 13:11:26 +0800

    fix

--- ts-ddd/dist/domain/common/Result.js
+++
@@ -1,3 +0,0 @@
-export const Ok = (value) => ({ ok: true, value });
-export const Err = (error) => ({ ok: false, error });
-//# sourceMappingURL=Result.js.map
--- ts-ddd/dist/domain/payment/PaymentStatus.js
+++
@@ -1,8 +0,0 @@
-export var DomainPaymentStatus;
-(function (DomainPaymentStatus) {
-    DomainPaymentStatus["PENDING"] = "PENDING";
-    DomainPaymentStatus["PAID"] = "PAID";
-    DomainPaymentStatus["CANCELED"] = "CANCELED";
-    DomainPaymentStatus["REFUNDED"] = "REFUNDED";
-})(DomainPaymentStatus || (DomainPaymentStatus = {}));
-//# sourceMappingURL=PaymentStatus.js.map
